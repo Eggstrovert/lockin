919f41d53497b442147cf657ebe1f12649270501
Eggstrovert <krljanovicdavid@gmail.com>
2024-03-23 23:22:09 -0700

    init push

---
+++ lockin/src/test/java/mountainmadness2024/lockin/LockinApplicationTests.java
@@ -0,0 +1,13 @@
+package mountainmadness2024.lockin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
