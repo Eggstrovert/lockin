439bb20dcfd32c172a474e389afe4ca90338cbe6
Eggstrovert <krljanovicdavid@gmail.com>
2024-03-24 00:03:44 -0700

    init push

---
+++ src/main/java/mountainmadness2024/lockin/LockinApplication.java
@@ -0,0 +1,13 @@
+package mountainmadness2024.lockin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockinApplication.class, args);
+	}
+
+}
